ff694b5f173723494fb1e448513e90424acd66d8
natsume37 <natsume37@outlook.com>
2023-10-31 15:24:27 +0800

    更改主题

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
 from flask import Flask, render_template
-from flask_bootstrap import Bootstrap5
+from flask_bootstrap import Bootstrap4
 
 import settings
 
 app = Flask(__name__)
 # app.config.from_object(settings)
-bootstrap = Bootstrap5(app)
+bootstrap = Bootstrap4(app)
+
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return render_template("index.html")
+    return render_template("test.xml")
+    # return "你好"
 
 
 @app.route("/get_data", methods=['post'])
 def data():
     return "登录成功"
+
 
 @app.route("/about")
 def about():
@@ -23,4 +26,3 @@ def about():
 
 if __name__ == '__main__':
     app.run()
-
