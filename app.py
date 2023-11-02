41637f24053b6cda19a194fac4390794a1cfe421
natsume37 <natsume37@outlook.com>
2023-11-02 17:17:06 +0800

    添加闪现信息功能、优化交互体验

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for,jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify,flash
 from flask_bootstrap import Bootstrap4
 from user import *
 from userLlist import *
@@ -7,6 +7,7 @@ from songs import *
 from func_list import *
 
 app = Flask(__name__)
+app.secret_key = 'natsume37'
 # app.config.from_object(settings)
 bootstrap = Bootstrap4(app)
 
@@ -16,8 +17,7 @@ def index():  # put application's code here
     fire_songs = songs(url=urls_dic["热歌榜"])
     biaos = songs(url=urls_dic["飙升榜"])
     yuans = songs(url=urls_dic["新歌榜"])
-
-    return render_template("index.html", fire_songs=fire_songs,biaos=biaos,yuans=yuans)
+    return render_template("index.html", fire_songs=fire_songs, biaos=biaos, yuans=yuans)
     # return "你好"
 
 
@@ -36,6 +36,7 @@ def login_egypt():
         if res[0]:
             return redirect(url_for('index'))
         else:
+            flash("用户名或密码错误请重新登录！")
             return redirect(url_for('login'))
 
 
@@ -54,8 +55,10 @@ def user_add():
         res = user.save_list(name, password)
         if res[0]:
             logger.debug("注册成功！")
+            flash("注册成功")
             return redirect(url_for('login'))
         else:
+            flash("注册失败、该用户名已注册")
             return redirect(url_for('register'))
 
 
@@ -64,10 +67,19 @@ def search_music(music_name):
     songs = clean_json(music_name)
     return render_template('search.html', **locals())
 
+
 # @app.route("/search/<music_name>")
 # def search_music(music_name):
 #     songs = clean_json(music_name)
 #     return render_template('search.html', **locals())
+
+@app.route("/fire")
+def fire():
+    fire_songs = songs(url=urls_dic["热歌榜"],)
+    biaos = songs(url=urls_dic["飙升榜"])
+    yuans = songs(url=urls_dic["新歌榜"])
+
+    return render_template("fire_file.html", fire_songs=fire_songs, biaos=biaos, yuans=yuans)
 
 
 if __name__ == '__main__':
--- static/js/my_js.js
+++ static/js/my_js.js
@@ -1,23 +1,23 @@
 // foot
-      window.onscroll = function () {
-        scrollFunction();
-      };
+window.onscroll = function () {
+    scrollFunction();
+};
 
-      function scrollFunction() {
-        if (
-          document.body.scrollTop > 10 ||
-          document.documentElement.scrollTop > 10
-        ) {
-          // 显示返回顶部按钮
-          document.getElementById("backToTopBtn").style.display = "block";
-        } else {
-          // 隐藏返回顶部按钮
-          document.getElementById("backToTopBtn").style.display = "none";
-        }
-      }
+function scrollFunction() {
+if (
+  document.body.scrollTop > 10 ||
+  document.documentElement.scrollTop > 10
+) {
+  // 显示返回顶部按钮
+  document.getElementById("backToTopBtn").style.display = "block";
+} else {
+  // 隐藏返回顶部按钮
+  document.getElementById("backToTopBtn").style.display = "none";
+}
+}
 
-      // 平滑滚动到页面顶部
-      function scrollToTop() {
-        document.body.scrollTop = 0; // Safari
-        document.documentElement.scrollTop = 0; // Chrome, Firefox, IE and Opera
-      }
+// 平滑滚动到页面顶部
+function scrollToTop() {
+document.body.scrollTop = 0; // Safari
+document.documentElement.scrollTop = 0; // Chrome, Firefox, IE and Opera
+}
